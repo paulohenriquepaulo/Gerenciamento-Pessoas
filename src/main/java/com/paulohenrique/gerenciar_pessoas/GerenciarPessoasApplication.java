fbcfdea87a170f6451780eb1f0268583ecc79562
paulohenriquepaulo <p.henrique461@gmail.com>
2023-01-20 10:37:13 -0300

    Inicio projeto Gerenciamento Pessoas

---
+++ src/main/java/com/paulohenrique/gerenciar_pessoas/GerenciarPessoasApplication.java
@@ -0,0 +1,13 @@
+package com.paulohenrique.gerenciar_pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarPessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarPessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulohenrique/gerenciar_pessoas/GerenciarPessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulohenrique.gerenciar_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarPessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
